811e0f98e02a8f1ec529ef0a55c077acfda63563
umihico <umihico@users.noreply.github.com>
2019-01-19 11:43:40 +0900

    refactoring

--- chromeless/chromeless/dump_code.py
+++ chromeless/chromeless/dump_code.py
@@ -3,15 +3,6 @@ import marshal
 import pickle
 
 
-def load_code(hex_pickled_data):
-    pickled_data = bytes([int(hex_pickled_data[i:i+2], 16)
-                          for i in range(0, len(hex_pickled_data), 2)])
-    funcname, marshaled_code, arg, kwargs = pickle.loads(pickled_data)
-    code = marshal.loads(marshaled_code)
-    func = types.FunctionType(code, globals(), funcname)
-    return func, arg, kwargs
-
-
 def dump_code(func, arg, kwargs):
     byte_func = marshal.dumps(func.__code__)
     funcname = func.__name__
@@ -22,10 +13,19 @@ def dump_code(func, arg, kwargs):
 
 
 def test():
+    def _load_code(hex_pickled_data):
+        """copied from awslambda dir"""
+        pickled_data = bytes([int(hex_pickled_data[i:i+2], 16)
+                              for i in range(0, len(hex_pickled_data), 2)])
+        funcname, marshaled_code, arg, kwargs = pickle.loads(pickled_data)
+        code = marshal.loads(marshaled_code)
+        func = types.FunctionType(code, globals(), funcname)
+        return func, arg, kwargs
+
     def method(i):
         return i*10
     funcdata = dump_code(method, (10,), {})
-    reborn_func, arg, kwargs = load_code(funcdata)
+    reborn_func, arg, kwargs = _load_code(funcdata)
     print(reborn_func(*arg, **kwargs))
 
 
