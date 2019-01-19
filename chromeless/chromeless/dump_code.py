59080dc113cee0179aed8ddc77dd081718bcd4a5
umihico <umihico@users.noreply.github.com>
2019-01-19 11:41:38 +0900

    version 0.1.0 fix error

--- chromeless/chromeless/dump_code.py
+++ chromeless/chromeless/dump_code.py
@@ -1,7 +1,15 @@
 import types
 import marshal
 import pickle
-from awslambda import load_code
+
+
+def load_code(hex_pickled_data):
+    pickled_data = bytes([int(hex_pickled_data[i:i+2], 16)
+                          for i in range(0, len(hex_pickled_data), 2)])
+    funcname, marshaled_code, arg, kwargs = pickle.loads(pickled_data)
+    code = marshal.loads(marshaled_code)
+    func = types.FunctionType(code, globals(), funcname)
+    return func, arg, kwargs
 
 
 def dump_code(func, arg, kwargs):
