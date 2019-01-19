7b2285ba31ff8e2e1713e0b10bcf36c7aea4e57a
umihico <umihico@users.noreply.github.com>
2019-01-19 19:46:21 +0900

    automatically adjust deploy place

--- apigateway_credentials-example.py
+++ apigateway_credentials-example.py
@@ -1,2 +1,6 @@
+from subprocess import check_output
+current_branch_name = check_output(
+    "git symbolic-ref --short HEAD".split(" ")).decode().split('\n')[0]
+
 awsgateway_apikey = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-awsgateway_url = "https://XXXXXXXXXX.execute-api.us-west-2.amazonaws.com/default/chromeless"
+awsgateway_url = "https://XXXXXXXXXX.execute-api.us-west-2.amazonaws.com/default/chromeless-" + current_branch_name
--- awslambda/deploy.sh
+++ awslambda/deploy.sh
@@ -1,2 +1,3 @@
 rm -rf deploy_package.zip
-sudo docker run -v "${PWD}":/var/task chromeless && aws lambda update-function-code  --publish --profile lambda_deployer --function-name chromeless --zip-file fileb://deploy_package.zip
+git_branch_name=$(git symbolic-ref --short HEAD)
+sudo docker run -v "${PWD}":/var/task chromeless && aws lambda update-function-code  --publish --profile lambda_deployer --function-name chromeless-${git_branch_name} --zip-file fileb://deploy_package.zip
