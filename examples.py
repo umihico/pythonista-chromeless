06b4dfd9e76ff0fa6011fd06d954c520930fc5ad
umihico <umihico@users.noreply.github.com>
2019-01-19 18:01:53 +0900

    update examples

--- examples.py
+++ examples.py
@@ -1,3 +1,4 @@
+from apigateway_credentials import awsgateway_url, awsgateway_apikey
 from pypi.chromeless.chromeless import Chromeless
 
 
@@ -6,8 +7,26 @@ def get_title(self, url):
     return self.title
 
 
-def get_as_list(self):
+def get_list(self):
     self.get("https://stackoverflow.com/")
     question_titles = [e.text for e in self.find_elements_by_xpath(
         "//h3/a[@class='question-hyperlink']")]
     return question_titles
+
+
+def run_examples():
+    chrome = Chromeless(awsgateway_url, awsgateway_apikey)
+
+    chrome.attach_method(get_title)
+    result = chrome.get_title("https://google.com")
+    print(result, type(result))
+    # Google <class 'str'>
+
+    chrome.attach_method(get_list)
+    result = chrome.get_list()
+    print(result[:2], type(result))
+    # ['JSoup: how to list links from a list?', 'How to Fix PHPSESSID issue in Symfony 3.4'] <class 'list'>
+
+
+if __name__ == '__main__':
+    run_examples()
