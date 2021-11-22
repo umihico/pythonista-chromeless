d044c25a58649680bde6b44a45900f4e2bbb5eb4
umihico <umihico@users.noreply.github.com>
2021-11-22 22:53:58 +0900

    feat: use circleci

--- build.sh
+++ build.sh
@@ -1,74 +1,37 @@
 #!/bin/bash
-set -euxo pipefail
+set -eo pipefail
 
-PKG_NAME=chromeless
-DIR_NAME=$PKG_NAME
-AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id)
-AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key)
-AWS_REGION=$(aws configure get region)
+circleci local execute --job pytest-local
 
-echo "TEST LOCALLY"
-DOCKER_BUILDKIT=1 docker build -t chromeless .
-docker-compose -f sls/docker-compose.yml up --exit-code-from pytest --abort-on-container-exit
+cat <<EOF > .env
+-e AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id)
+-e AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key)
+-e AWS_REGION=$(aws configure get region)
+-e AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+-e AWS_DEFAULT_REGION=$(aws configure get region)
+-e PYPI_USERNAME_DEV=$(aws ssm get-parameter --name PYPI_USERNAME_TEST --query 'Parameter.Value' --output text)
+-e PYPI_PASSWORD_DEV=$(aws ssm get-parameter --name PYPI_PASSWORD_TEST --query 'Parameter.Value' --output text)
+-e PYPI_USERNAME_PROD=$(aws ssm get-parameter --name PYPI_USERNAME_PROD --query 'Parameter.Value' --output text)
+-e PYPI_PASSWORD_PROD=$(aws ssm get-parameter --name PYPI_PASSWORD_PROD --query 'Parameter.Value' --output text)
+EOF
 
-echo "DEPLOY STAGING LAMBDA"
-sls deploy --stage stg --region $AWS_REGION
+circleci local execute --job sls-dev $(cat .env)
+circleci local execute --job pypi-dev $(cat .env)
 
-echo "DEPLOY STAGING PYPI"
-docker build . -t pypienv_chromeless -f pypi/Dockerfile
-touch chromeless/__version__.py
-PYPI_USERNAME_TEST=$(aws ssm get-parameter --name PYPI_USERNAME_TEST --query 'Parameter.Value' --output text)
-PYPI_PASSWORD_TEST=$(aws ssm get-parameter --name PYPI_PASSWORD_TEST --query 'Parameter.Value' --output text)
-docker run --rm \
-	--mount type=bind,source=$(pwd)/chromeless/__version__.py,target=/app/chromeless/__version__.py \
-	-e PKG_NAME=$PKG_NAME \
-  -e DIR_NAME=$DIR_NAME \
-  -e PYPI_USERNAME=$PYPI_USERNAME_TEST \
-  -e PYPI_PASSWORD=$PYPI_PASSWORD_TEST \
-  -e REPOSITORY=testpypi \
-  -e STAGE=stg \
-	pypienv_chromeless
+STAGE=dev
+cat <<EOF >> .env
+-e API_URL_DEV=$(aws cloudformation describe-stacks --stack-name chromeless-$STAGE --query "Stacks[0].Outputs[?OutputKey=='ServiceEndpoint'].OutputValue" --output text)
+-e API_KEY_DEV=$(aws apigateway get-api-keys --query 'items[?name==`chromeless-apikey-'$STAGE'`].value' --include-values --output text)
+EOF
 
-echo "TEST STAGING"
-STAGE=stg
-STACK_NAME=chromeless-$STAGE
-API_URL=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='ServiceEndpoint'].OutputValue" --output text)
-API_KEY=$(aws apigateway get-api-keys --query 'items[?name==`chromeless-apikey-'$STAGE'`].value' --include-values --output text)
-CHROMELESS_SERVER_FUNCTION_NAME=chromeless-server-stg \
-  AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
-  AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
-  AWS_REGION=$AWS_REGION \
-  API_URL=$API_URL \
-  API_KEY=$API_KEY \
-  docker-compose -f pytest/docker-compose.yml up
+circleci local execute --job pytest-dev $(cat .env)
+circleci local execute --job sls-prod $(cat .env)
+circleci local execute --job pypi-prod $(cat .env)
 
-echo "DEPLOY PRODUCTION LAMBDA"
-sls deploy --region $AWS_REGION
-
-echo "DEPLOY PRODUCTION PYPI"
-docker build . -t pypienv_chromeless -f pypi/Dockerfile
-touch chromeless/__version__.py
-PYPI_USERNAME_PROD=$(aws ssm get-parameter --name PYPI_USERNAME_PROD --query 'Parameter.Value' --output text)
-PYPI_PASSWORD_PROD=$(aws ssm get-parameter --name PYPI_PASSWORD_PROD --query 'Parameter.Value' --output text)
-docker run --rm \
-	--mount type=bind,source=$(pwd)/chromeless/__version__.py,target=/app/chromeless/__version__.py \
-	-e PKG_NAME=$PKG_NAME \
-  -e DIR_NAME=$DIR_NAME \
-  -e PYPI_USERNAME=$PYPI_USERNAME_PROD \
-  -e PYPI_PASSWORD=$PYPI_PASSWORD_PROD \
-  -e REPOSITORY=pypi \
-  -e STAGE=prod \
-	pypienv_chromeless
-
-echo "TEST PRODUCTION"
 STAGE=prod
-STACK_NAME=chromeless-$STAGE
-API_URL=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='ServiceEndpoint'].OutputValue" --output text)
-API_KEY=$(aws apigateway get-api-keys --query 'items[?name==`chromeless-apikey-'$STAGE'`].value' --include-values --output text)
-CHROMELESS_SERVER_FUNCTION_NAME=chromeless-server-prod \
-  AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
-  AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
-  AWS_REGION=$AWS_REGION \
-  API_URL=$API_URL \
-  API_KEY=$API_KEY \
-  docker-compose -f pytest/docker-compose.yml up
+cat <<EOF >> .env
+-e API_URL_PROD=$(aws cloudformation describe-stacks --stack-name chromeless-$STAGE --query "Stacks[0].Outputs[?OutputKey=='ServiceEndpoint'].OutputValue" --output text)
+-e API_KEY_PROD=$(aws apigateway get-api-keys --query 'items[?name==`chromeless-apikey-'$STAGE'`].value' --include-values --output text)
+EOF
+
+circleci local execute --job pytest-prod $(cat .env)
--- pytest/docker-entrypoint.sh
+++ pytest/docker-entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -euxo pipefail
 
-if [ "$(pwd)" = "/latest" ]; then
-  until $(python /local/chromeless/__version__.py); do sleep 5; done
-fi
-
 cp /tests.py ./
 cp /example.py ./
 pytest tests.py -ra
