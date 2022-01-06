dc0195fe9a6db1acb966c2d24576effc7adac197
umihico <umihico@users.noreply.github.com>
2022-01-07 07:57:11 +0900

    fix

--- sls/versions/server_ver_1.py
+++ sls/versions/server_ver_1.py
@@ -1,3 +1,4 @@
+from tempfile import mkdtemp
 import types
 from selenium import webdriver
 from picklelib import dumps  # imports in Dockerfile
@@ -49,15 +50,16 @@ class ChromelessServer():
 def get_default_options():
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
     options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1280x1696")
-    options.add_argument("--disable-application-cache")
-    options.add_argument("--disable-infobars")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--hide-scrollbars")
-    options.add_argument("--enable-logging")
-    options.add_argument("--log-level=0")
     options.add_argument("--single-process")
-    options.add_argument("--ignore-certificate-errors")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-dev-tools")
+    options.add_argument("--no-zygote")
+    options.add_argument("--remote-debugging-port=9222")
     options.add_argument("--homedir=/tmp")
-    return options
+    options.add_argument(f"--homedir={mkdtemp()}")
+    options.add_argument(f"--user-data-dir={mkdtemp()}")
+    options.add_argument(f"--data-path={mkdtemp()}")
+    options.add_argument(f"--disk-cache-dir={mkdtemp()}")
